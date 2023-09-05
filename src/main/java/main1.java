91b6bf1b214e45ae620b10536dc2863fa049a616
200gram <ida.laak@gmail.com>
2023-09-05 14:15:35 +0200

    Initial commit

---
+++ src/main/java/main1.java
@@ -0,0 +1,11 @@
+
+
+public class main1 {
+
+	public static int test1() {
+		return 2 + 2;
+	}
+
+}
+
+
---
+++ src/main/java/test1.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class test1 {
+
+	@Test
+	public void test2() {
+		assertEquals(4, main1.test1());
+	}
+}
